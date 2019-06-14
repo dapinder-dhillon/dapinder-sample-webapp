504183f14dc045ee2367acd890050cd7c0c8fe16
SING22 <d.singh.3@elsevier.com>
2019-06-14 16:57:16 +0100

    sample spring web project skeleton

---
+++ src/main/java/com/dapinder/web/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.dapinder.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home(){
+        return "Welcome to Dapinder Web Home!!";
+    }
+
+    @GetMapping("/health")
+    public String health(){
+        return "up";
+    }
+}
